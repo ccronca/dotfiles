2c2b41ce83b893811c501979399f2fb22e5bb7da
Camilo Cota <ccota@redhat.com>
2025-03-03 18:06:35 +0100

    Add initial zshrc and aliases

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -7,7 +7,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="vimrc tmux.conf gitignore"    # list of files/folders to symlink in homedir
+files="vimrc tmux.conf gitignore zshrc aliases"    # list of files/folders to symlink in homedir
 
 ##########
 
