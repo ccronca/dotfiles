72914ccabe458aab8e36bb6e4397ffaff03e5551
Camilo Cota <ccota@redhat.com>
2025-03-09 18:43:40 +0100

    Add basic installation of vim plugins

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -1,27 +1,21 @@
 #!/bin/bash
 ############################
 # This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
+# and install vim plugins
 ############################
 
-########## Variables
-
-dir=~/dotfiles                    # dotfiles directory
-olddir=~/dotfiles_old             # old dotfiles backup directory
+dir=~/dotfiles                    
+olddir=~/dotfiles_old             
 files="vimrc tmux.conf gitignore zshrc aliases gitconfig bashrc bash_logout bash_profile ctags.d"    # list of files/folders to symlink in homedir
 
-##########
-
-# create dotfiles_old in homedir
 echo "Creating $olddir for backup of any existing dotfiles in ~"
 mkdir -p $olddir
 echo "...done"
 
-# change to the dotfiles directory
 echo "Changing to the $dir directory"
 cd $dir
 echo "...done"
 
-# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
 for file in $files; do
     echo "Moving any existing dotfiles from ~ to $olddir"
     mv ~/.$file ~/dotfiles_old/
@@ -34,3 +28,27 @@ DIRS=("$HOME/.vim/backups" "$HOME/.vim/swaps" "$HOME/.vim/undo")
 for dir in "${DIRS[@]}"; do
     mkdir -p "$dir"
 done
+
+PLUGIN_DIR="$HOME/.vim/pack/plugins/start"
+
+declare -A PLUGINS=(
+    ["ctrlp.vim"]="https://github.com/ctrlpvim/ctrlp.vim.git"
+)
+
+mkdir -p "$PLUGIN_DIR"
+
+echo "Managing Vim plugins in $PLUGIN_DIR..."
+
+for plugin in "${!PLUGINS[@]}"; do
+    PLUGIN_PATH="$PLUGIN_DIR/$plugin"
+
+    if [ -d "$PLUGIN_PATH/.git" ]; then
+        echo "Updating $plugin..."
+        git -C "$PLUGIN_PATH" pull --ff-only
+    else
+        echo "Installing $plugin..."
+        git clone --depth=1 "${PLUGINS[$plugin]}" "$PLUGIN_PATH"
+    fi
+done
+
+echo "Vim plugins management complete!"
